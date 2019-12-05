1ddc5532bdc475c1571fc2d8f30a9add9fc8f236
UncleJerry <jared.myhrberg@gmail.com>
2019-12-04 16:04:56 -0800

    post route working

--- test/routes/rout.test.js
+++ test/routes/rout.test.js
@@ -1,26 +1,36 @@
-// require('dotenv').config();
-// const request = require('supertest');
-// const app = require('../../lib/app');
-// const mongoose = require('mongoose');
-// const connect = require('../../lib/utils/connect');
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../../lib/app');
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
 
-// beforeAll(() => connect());
-// beforeEach(() => mongoose.connection.dropDatabase());
-// afterAll(() => mongoose.connection.close());
+beforeAll(() => connect());
+beforeEach(() => mongoose.connection.dropDatabase());
+afterAll(() => mongoose.connection.close());
 
-// describe('route tests', () => {
+describe('route tests', () => {
 
-//   it('create a new mod', () => {
-//     return request(app)
-//       .post('/api/v1/rout')
-//       .send({
-//         name: 'jimmy'
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           name: 'jimmy',
-//           _id: expect.any(String),
-//         });
-//       });
-//   });
-// });
+  it('create a new mod', () => {
+    return request(app)
+      .post('/api/v1/rout')
+      .send({
+        name: 'jimmy',
+        score: 100,
+        totalTime: 3600,
+        timeOfDay: 1620,
+        location: '1234321342',
+        ip: '12345432'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'jimmy',
+          score: 100,
+          totalTime: 3600,
+          timeOfDay: 1620,
+          location: '1234321342',
+          ip: '12345432'
+        });
+      });
+  });
+});
