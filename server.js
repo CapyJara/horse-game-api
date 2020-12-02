9563e13f63f11da3fcd8895a48bf10d4ece05b50
jared <jared.myhrberg@gmail.com>
2020-12-01 19:46:23 -0800

    changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
+const ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'dev';
+require('dotenv').config({ path: `./.env.${ENV}` });
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
-const PORT = process.env.PORT || 7891;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
